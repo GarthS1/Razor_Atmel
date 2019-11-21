d453774dce5e9cd463dab89feffc883f4fe0aa5c
Garth Slaney <45348251+GarthS1@users.noreply.github.com>
2019-11-20 20:07:07 -0700

    made the code more modular and much simpler

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -1,22 +1,10 @@
 /**********************************************************************************************************************
 File: user_app1.c                                                                
 
-----------------------------------------------------------------------------------------------------------------------
-To start a new task using this user_app1 as a template:
- 1. Copy both user_app1.c and user_app1.h to the Application directory
- 2. Rename the files yournewtaskname.c and yournewtaskname.h
- 3. Add yournewtaskname.c and yournewtaskname.h to the Application Include and Source groups in the IAR project
- 4. Use ctrl-h (make sure "Match Case" is checked) to find and replace all instances of "user_app1" with "yournewtaskname"
- 5. Use ctrl-h to find and replace all instances of "UserApp1" with "YourNewTaskName"
- 6. Use ctrl-h to find and replace all instances of "USER_APP1" with "YOUR_NEW_TASK_NAME"
- 7. Add a call to YourNewTaskNameInitialize() in the init section of main
- 8. Add a call to YourNewTaskNameRunActiveState() in the Super Loop section of main
- 9. Update yournewtaskname.h per the instructions at the top of yournewtaskname.h
-10. Delete this text (between the dashed lines) and update the Description below to describe your task
-----------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------
 
 Description:
-This is a user_app1.c file template 
+Code to enter and set a password. 
 
 ------------------------------------------------------------------------------------------------------------------------
 API:
@@ -42,7 +30,7 @@ All Global variable names shall start with "G_UserApp1"
 ***********************************************************************************************************************/
 /* New variables */
 volatile u32 G_u32UserApp1Flags;                       /* Global state flags */
-volatile int G_UserApp1password[11] ={1,2,3,0};       /* Password to match where -1 is the end */
+volatile int G_UserApp1password[11];       /* Password to match where 0 is the end */
 
 
 /*--------------------------------------------------------------------------------------------------------------------*/
@@ -70,6 +58,144 @@ Function Definitions
 /* Public functions                                                                                                   */
 /*--------------------------------------------------------------------------------------------------------------------*/
 
+/* resets the password array for next attempt*/
+void reset(int* reset, int password[])
+{	
+  if(WasButtonPressed(BUTTON0) || WasButtonPressed(BUTTON1) || WasButtonPressed(BUTTON2) || WasButtonPressed(BUTTON3) && !IsButtonPressed(BUTTON0) && !IsButtonPressed(BUTTON1) && !IsButtonPressed(BUTTON2) && !IsButtonPressed(BUTTON3) )
+  {
+    ButtonAcknowledge(BUTTON0);
+    ButtonAcknowledge(BUTTON1);
+    ButtonAcknowledge(BUTTON2);
+    ButtonAcknowledge(BUTTON3);
+    *reset = 0;
+    for(int i = 0; password[i] != 0; i++)
+    {
+      password[i] = 0;
+    }
+  }
+}
+
+/* when trying to enter a password */
+void enter(int *state)
+{
+	static int password[10]; //stores the vaules of the buttons passed 
+	static int point = 0; //stores the location of the array 
+  static int clockCounter = 0; //counter for blinking 
+	
+  LCDCommand(LCD_CLEAR_CMD);
+	static u8 au8Message[] = "ENTERING A PASSWORD   ";
+  LCDMessage(LINE1_START_ADDR, au8Message);
+	
+  if(point == -1)
+  {
+    if(clockCounter == 1000)
+    {
+      LedOn(RED);
+      clockCounter = 0;
+    }
+    if(clockCounter == 500)
+    {
+      LedOff(RED);
+    }
+    clockCounter++;
+    reset(&point, password);
+  }
+  else if(point == -2)
+  {
+    if(clockCounter == 1000)
+    {
+      LedOn(GREEN);
+      clockCounter = 0;
+    }
+    if(clockCounter == 500)
+    {
+      LedOff(GREEN);
+    }
+    
+    clockCounter++;
+    LedOff(RED);
+    reset(&point, password);
+  }
+  else
+  {
+		LedOn(RED);
+    LedOff(GREEN); 
+   
+    if(WasButtonPressed(BUTTON0) && !IsButtonPressed(BUTTON0))
+    {
+			ButtonAcknowledge(BUTTON0);
+      password[point] = 1;
+      point++;
+		}   
+   
+    if(WasButtonPressed(BUTTON1) && !IsButtonPressed(BUTTON1))
+    {
+      ButtonAcknowledge(BUTTON1);
+      password[point] = 2;
+      point++;
+    }
+      
+    if(WasButtonPressed(BUTTON2) && !IsButtonPressed(BUTTON2) )
+    {
+      ButtonAcknowledge(BUTTON2);
+      password[point] = 3;
+      point++;
+    }
+  
+    if(WasButtonPressed(BUTTON3) && !IsButtonPressed(BUTTON3))
+    {
+			ButtonAcknowledge(BUTTON3);
+      for(int i = 0; i  < 10; i++)
+      {
+				if(password[i] != G_UserApp1password[i])
+        {
+					point = -1;  
+					break;
+				}
+				point = -2;
+			}
+    }
+  }
+} /* end of enter */	
+
+/* when trying to set a password */
+void set(int* state)
+{
+  static u8 au8Message[] = "SETTING A PASSWORD     ";
+	static int point = 0;
+  
+  LCDMessage(LINE1_START_ADDR, au8Message);
+	
+	if(WasButtonPressed(BUTTON0) && !IsButtonPressed(BUTTON0))
+  {
+		ButtonAcknowledge(BUTTON0);
+		G_UserApp1password[point] = 1;
+    point++;
+	}   
+   
+  if(WasButtonPressed(BUTTON1) && !IsButtonPressed(BUTTON1))
+  {
+    ButtonAcknowledge(BUTTON1);
+    G_UserApp1password[point] = 2;
+    point++;
+  }
+      
+  if(WasButtonPressed(BUTTON2) && !IsButtonPressed(BUTTON2) )
+  {
+    ButtonAcknowledge(BUTTON2);
+    G_UserApp1password[point] = 3;
+    point++;
+  }
+  
+  if(WasButtonPressed(BUTTON3) && !IsButtonPressed(BUTTON3))
+  {
+	  ButtonAcknowledge(BUTTON3);
+	  *state = 1;
+	  point = 0;
+  }
+}
+	
+	
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Protected functions                                                                                                */
 /*--------------------------------------------------------------------------------------------------------------------*/
@@ -142,100 +268,19 @@ State Machine Function Definitions
 **********************************************************************************************************************/
 
 /*-------------------------------------------------------------------------------------------------------------------*/
-/* Wait for ??? */
 static void UserApp1SM_Idle(void)
 {
-  static int password[10]; //stores the vaules of the buttons passed 
-  static int point = 0; //stores the location of the array 
-  static int clockCounter = 0; //counter for blinking 
- 
-  if(point == -1){
-    if(clockCounter == 1000){
-      LedOn(RED);
-      clockCounter = 0;
-    }
-    if(clockCounter == 500){
-      LedOff(RED);
-    }
-    clockCounter++;
-    if(WasButtonPressed(BUTTON0) || WasButtonPressed(BUTTON1) || WasButtonPressed(BUTTON2) || WasButtonPressed(BUTTON3)){
-      if(!IsButtonPressed(BUTTON0) && !IsButtonPressed(BUTTON1) && !IsButtonPressed(BUTTON2) && !IsButtonPressed(BUTTON3)){
-        ButtonAcknowledge(BUTTON0);
-        ButtonAcknowledge(BUTTON1);
-        ButtonAcknowledge(BUTTON2);
-        ButtonAcknowledge(BUTTON3);
-        point = 0;
-        for(int i = 0; password[i] != 0; i++){
-          password[i] = 0;
-        }
-      }
-    }   
-  }
-  else if(point == -2){
-    if(clockCounter == 1000){
-      LedOn(GREEN);
-      clockCounter = 0;
-    }
-    if(clockCounter == 500){
-      LedOff(GREEN);
-    }
-    clockCounter++;
-    LedOff(RED);
-    
-    if(WasButtonPressed(BUTTON0) || WasButtonPressed(BUTTON1) || WasButtonPressed(BUTTON2) || WasButtonPressed(BUTTON3)){
-      if(!IsButtonPressed(BUTTON0) && !IsButtonPressed(BUTTON1) && !IsButtonPressed(BUTTON2) && !IsButtonPressed(BUTTON3)){
-        ButtonAcknowledge(BUTTON0);
-        ButtonAcknowledge(BUTTON1);
-        ButtonAcknowledge(BUTTON2);
-        ButtonAcknowledge(BUTTON3);
-        point = 0;
-        for(int i = 0; password[i] != 0; i++){
-          password[i] = 0;
-        }
-      }
-    }
-  }
-  else{
-   LedOn(RED);
-   LedOff(GREEN); 
-   
-   if(WasButtonPressed(BUTTON0)){
-     if(!IsButtonPressed(BUTTON0)){
-       ButtonAcknowledge(BUTTON0);
-       password[point] = 1;
-       point++;
-     }
-  }
-     
-   if(WasButtonPressed(BUTTON1)){
-     if(!IsButtonPressed(BUTTON1)){
-       ButtonAcknowledge(BUTTON1);
-       password[point] = 2;
-       point++;
-     }
-  }
-      
-   if(WasButtonPressed(BUTTON2)){
-     if(!IsButtonPressed(BUTTON2)){
-       ButtonAcknowledge(BUTTON2);
-       password[point] = 3;
-       point++;
-     }
-  }
-  
-   if(WasButtonPressed(BUTTON3)){
-     if(!IsButtonPressed(BUTTON3)){
-       ButtonAcknowledge(BUTTON3);
-       for(int i = 0; i  < 10; i++){
-         if(password[i] != G_UserApp1password[i]){
-           point = -1;  
-           break;
-         }
-         point = -2;
-        }
-      }
-   }
-   } 
+	static int state = 0;  //keeps track of which state the board is in 
+	
+	if( state == 0 )
+	{
+		set(&state);
+	}
+	else
+	{
+		enter(&state);
+	}
+
 } /* end UserApp1SM_Idle() */
     
 
